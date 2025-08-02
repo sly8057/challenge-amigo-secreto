203f0954b8a020ccb75b09cb7d3a15aca1542ff3
eri <a22310383@ceti.mx>
2025-08-02 15:08:06 -0600

    agrega amigos desde el input

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ let amigos =[]
 function limpiarCampo(id) {
     document.querySelector(`#${id}`).value = "";
 }
+
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value.trim();
+    if(!amigo) {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+    amigos.push(amigo);
+    limpiarCampo("amigo");
+}
